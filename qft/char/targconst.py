0a633d2973f1b349c783df9cbec22b1ea215509c
TheSpeks <thespeks@users.noreply.github.com>
2014-05-02 12:12:04 -0400

    Create targconst.py

---
+++ qft/char/targconst.py
@@ -0,0 +1,98 @@
+
+# Peener
+COCK_LEN =      '000'
+COCK_BASE_DIA = '001' # base diameter
+COCK_TIP_DIA =  '002' # tip diameter
+COCK_TYPE =     '003'
+COCK_MUSK =     '004'
+COCK_SENS =     '005' # sensitivity
+COCK_HARD =     '006' # Hardness
+COCK_VARS = (
+    COCK_BASE_DIA, COCK_TIP_DIA, COCK_LEN, COCK_TYPE, COCK_MUSK, COCK_SENS, COCK_HARD)
+
+# Sheath        (sheath can exist under foreskin)
+SHEATH_TIGHT =  '020'
+SHEATH_VARS =   (SHEATH_TIGHT)
+
+# Foreskin      
+FSKIN_TIGHT =   '040'
+FSKIN_THICK =   '041' 
+FSKIN_TRANSL =  '042' # translucent
+FSKIN_MUSK =    '043' 
+FSKIN_VARS =    (FSKIN_TIGHT, FSKIN_THICK, FSKIN_TRANSL, FSKIN_MUSK)
+
+# Ballz
+BALL_SIZE =     '060'
+BALL_INT =      '061' # bool
+BALL_VARS =     (BALL_SIZE', BALL_INT)
+
+# Scrot
+SCROT_TIGHT =   '080'
+SCROT_MUSK =    '081' 
+SCROT_VARS =    (SCROT_TIGHT, SCROT_MUSK)
+
+# Poon
+POON_MOIST =    '100'
+POON_TIGHT =    '101'
+POON_COL =      '102
+POON_VARS =     (POON_MOIST, POON_TIGHT, POON_COL)
+    
+# Clit
+CLIT_LEN =      '120'
+CLIT_SENS =     '121'
+CLIT_HARD =     '122'
+CLIT_VARS =     (CLIT_LEN, CLIT_SENS, CLIT_HARD)
+
+# Titz
+# -- First row
+BOOB_SIZE =     '140'
+BOOB_VARS =     (BOOB_SIZE)
+# -- 2nd row
+BOOB2_SIZE =    '150'
+BOOB2_VARS =    (BOOB2_SIZE)
+# -- Third row
+BOOB3_Size =    '160'
+BOOB3_VARS =    (BOOB3_SIZE)
+
+# Nipz
+NIP_LEN =       '170'
+NIP_COL =       '171'
+NIP_VARS =      (NIP_LEN, NIP_COL)
+    
+# Rump
+BUTT_SIZE =     '200'
+BUTT_TONE =     '201'
+BUTT_VARS =     (BUTT_SIZE, BUTT_TONE)
+
+# Anus
+ANUS_SIZE =     '210'
+ANUS_TIGHT =    '211'
+ANUS_COL =      '212'
+ANUS_VARS =     (ANUS_SIZE, ANUS_COL)
+
+
+# Tail
+TAIL_LEN =      '900'
+TAIL_TIP_COL =  '902'
+TAIL_VARS =     (TAIL_LEN, TAIL_TIP_COL)
+    
+# Ears
+EAR_TIP_COL_L = '920'
+EAR_TIP_COL_R = '921'
+EARS_VARS =     (EAR_TIP_COL_L, EAR_TIP_COL_R)
+
+# HAIR
+HAIR_LEN =      '930'
+HAIR_COL =      '931'
+HAIR_STYLE =    '932' # straight, wavy, curly, etc.
+HAIR_VARS =     (HAIR_LEN, HAIR_COL, HAIR_STYLE)
+
+# EYES
+EYE_COL_L =     '940'
+EYE_COL_R =     '941'
+EYE_VARS =      (EYE_COL_L, EYE_COL_R)
+
+
+
+
+    
