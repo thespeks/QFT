952d85df4f16e7de14e0bc9676a4cd9693b71657
TheSpeks <thespeks@users.noreply.github.com>
2014-05-02 10:48:07 -0400

    Create relationship.py

---
+++ qft/char/relationship.py
@@ -0,0 +1,16 @@
+
+RELATIONSHIP =  'Relationship'
+BASE_SCORE =    'Base Score'
+
+
+MIN_SCORE = -999
+MAX_SCORE = 999
+
+class Relationship:
+    def __init__(self, a, b):
+        self._items = {
+        BASE_SCORE =  0,
+            }
+        
+        
+    
