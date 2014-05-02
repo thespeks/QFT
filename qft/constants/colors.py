667637056eda92ca182590272734725cbdd095b4
TheSpeks <thespeks@users.noreply.github.com>
2014-05-02 11:42:47 -0400

    Create colors.py

---
+++ qft/constants/colors.py
@@ -0,0 +1,32 @@
+
+
+
+# Color prefixes
+PALE =      'pale'
+DULL =      'dull'
+LIGHT =     'light'
+BRIGHT =    'bright'
+DARK =      'dark'
+
+PINK =      'pink'
+PURPLE =    'purple'
+BLUE =      'blue'
+BLUEGREEN = 'blue-green'
+GREEN =     'green'
+ORANGE =    'orange'
+YELLOW =    'yellow'
+
+BLACK =     'black'
+CHARCOAL =  'charcoal'
+GRAY =      'gray'
+WHITE =     'white'
+
+# Earth Tones
+OLIVE =     'olive'
+HONEY =     'honey'
+BROWN =     'brown'
+
+# Metallic
+GOLD =      'gold'
+SILVER =    'silver'
+COPPER =    'copper'
