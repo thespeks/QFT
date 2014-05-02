bba3c576ffa5a9f9c4e8d78274df910a3638133c
TheSpeks <thespeks@users.noreply.github.com>
2014-05-02 11:43:10 -0400

    Update __init__.py

--- qft/char/__init__.py
+++ qft/char/__init__.py
@@ -1,5 +1,44 @@
 
+# Male Stats
+VERILITY =  'Verility'  # only valid if balls
+
+# Male Statuses
+RUT =       'Rut'
+
+# Female Stats  
+FERTILITY = 'Fertility' # only valid if ovaries
+
+# Female Statuses
+PREG =      'Pregnant'
+HEAT =      'Heat'
+
+# Basic Stats
+HEALTH =    'Health'
+LUST =      'Lust'
+
+# Addictions
+CUM_ADD =   'Cum Addiction'
+MILK_ADD =  'Milk Addiction'
+
+
+
 
 
 class Char:
-    pass
+    _items = {
+        HEALTH:     100,
+        }
+
+
+    @property
+    def health(self):
+        """The health stat for this character."""
+        return self['stat_health']
+        
+    @property
+    def lust(self):
+        """The lust stat for this character."""
+        return self.['stat_lust']
+        
+    
+    
