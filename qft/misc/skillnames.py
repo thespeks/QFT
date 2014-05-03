46727d0d444d6a31cc15b4e9546c56454e892013
TheSpeks <thespeks@users.noreply.github.com>
2014-05-03 10:31:45 -0400

    Create skillnames.py

---
+++ qft/misc/skillnames.py
@@ -0,0 +1,31 @@
+SKILL_LOCKPICK =    'Lockpick'
+SKILL_BARTEND =     'Bartend'
+SKILL_REPAIR =      'Repair'
+SKILL_HAGGLE =      'Haggling'
+
+# General
+SKILL_CREATIVITY =  'Creativity'
+
+# Weapon
+WSKILL_POLEARM =    'Polearms'
+WSKILL_BLUNT =      'Blunt Weapons'
+WSKILL_THROW =      'Thrown Weapons'
+WSKILL_BLADED =     'Bladed Weapons'
+
+# Dancing
+SKILL_POLE_DANCE =  'Pole Dancing'
+SKILL_EROTIC_DANCE = 'Erotic Dancing' # topless/nude
+SKILL_DANCE =       'Dancing'     # general dancing
+
+SKILL_TEASE =       'Tease'
+SKILL_SEDUCTION =   'Seduction'
+
+# Oral
+SKILL_FELL =        'Fellatio'
+SKILL_CUNN =        'Cunnilingus'
+SKILL_DEEPTHROAT =  'Deepthroating'
+
+SKILL_HANDJOB =     'Handjob'
+SKILL_FOOTJOB =     'Footjob'
+SKILL_KISS =        'Kissing'
+SKILL_MASSAGE =     'Massage'
