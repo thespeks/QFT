77583b9e3704f626a3e4de784e3133217669682e
TheSpeks <thespeks@users.noreply.github.com>
2014-05-03 10:12:34 -0400

    Create places.py

---
+++ qft/misc/places.py
@@ -0,0 +1,27 @@
+
+BAR =             'Bar'
+STRIP_BAR =       'Strip Bar'
+BROTHEL =         'Brothel'
+MASSAGE_PARLOR =  'Massage Parlor'  # massage skill
+
+# Shop Types
+REPAIR_SHOP =     'Repair Shop'   
+PAWN_SHOP =       'Pawn Shop'     
+LOCK_SMITH =      'Lock Smith'    # locksmith skill
+
+# Entertainment
+THEATRE =         'Theatre'
+
+# 
+LIBRARY =         'Library'   
+GARDEN =          'Garden'    
+PARK =            'Park'      
+
+HOSPITAL =        'Hospital'  # get healed here
+PHARMACY =        'Pharmacy'  # get medicines here
+
+
+
+
+
+    
