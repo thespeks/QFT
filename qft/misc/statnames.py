9f8cb4a7e7c0c587f203b8cc68cd1ffa7ebf7eb9
TheSpeks <thespeks@users.noreply.github.com>
2014-05-03 10:43:05 -0400

    Create statnames.py

---
+++ qft/misc/statnames.py
@@ -0,0 +1,11 @@
+
+
+STAT_AGIL =   'Agility'     
+STAT_FLEX =   'Flexibility'
+STAT_STRN =   'Strength'
+STAT_SPED =   'Speed'
+STAT_LIBD =   'Libido'
+STAT_HLTH =   'Health'
+STAT_ENRG =   'Energy'
+STAT_AROU =   'Arousal'
+STAT_STAM =   'Stamina'
