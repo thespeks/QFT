d304cb37d949c959a3f8f4a8942a2bc8c007982e
TheSpeks <thespeks@users.noreply.github.com>
2014-05-03 03:22:20 -0400

    Create locations.py

---
+++ qft/world/locations.py
@@ -0,0 +1,25 @@
+# from trpg.location import location_data
+
+
+
+
+
+
+# Location Names
+DELDORA = "Deldora"
+VERNE =   "Verne"
+TEMRIN =  "Temrin"
+
+    
+class Reg:
+    _north = None
+    _east = None
+    _south = None
+    _west = None
+    
+    
+class Region:
+    pass
+    
+class Town:
+    pass
