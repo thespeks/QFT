17ca54ba776a89d128598d68de71d606016965ae
TheSpeks <thespeks@users.noreply.github.com>
2014-05-04 01:03:43 -0400

    Update __init__.py

--- qft/char/__init__.py
+++ qft/char/__init__.py
@@ -12,36 +12,80 @@ FERTILITY = 'Fertility' # only valid if ovaries
 PREG =      'Pregnant'
 HEAT =      'Heat'
 
+# Other
+LACT =      'Lactation' # only if has nipples       
+
 # Basic Stats
 HEALTH =    'Health'
 LUST =      'Lust'
 
-# Addictions
-CUM_ADD =   'Cum Addiction'
-MILK_ADD =  'Milk Addiction'
 
 
+class Addiction(ValueDict):
+    __slots__ = ValueDict.__slots__, '_name')
+    def __init__(self, name):
+        self._name = name
+        self._items = {}
+    
+    @property
+    def name(self):
+        return self._name
+    
+CUM = 'Cum'
+MILK = 'Milk'
+ALCOHOL = 'Alcohol'
+    
+class AddictionTypes:
+    self._cum =     Addictions(CUM)
+    self._milk =    Addiction(MILK)
+    self._alcohol = Addictions(ALCOHOL)
+        
 
-
-
-class Char:
-    _items = {
-        STAT_AGIL: 10,
-        STAT_FLEX: 5,
-        STAT_STRN: 10,
-        STAT_SPED: 5,
-        STAT_LIBD: 10,
-        STAT_HLTH: 100,
-        STAT_ENRG: 100,
-        STAT_AROU: 0,
-        STAT_STAM: 50,
-        }
-
+class BaseChar:
+    __slots__ = '_skills', '_main_stats'
+    def __init__(self):
+        self._party =       None
+        self._skills =      ValueDict()
+        self._main_stats =  MainStats()
+        self._body =        ValueDict()
+        self._addictions =  AddictionTypes():
+        
+    
+    @property
+    def main_stats(self):
+        """Main stats for this character."""
+        return self._main_stats
+        
+    @property
+    def body(self):
+        return self._body
 
     @property
-    def health(self):
-        """The health stat for this character."""
-        return self['stat_health']
+    def gender(self):
+        return self._body.get_gender()
+
+    def has_skill(self, skill, min_level=1):
+        """Return True if this character has given skill of at least min_level"""
+        return self._skills.is_ge(skill, min_level)
         
+    
+class MainStats(ValueDict):
+    __slots__ = ValueDict.__slots__
+    def __init__(self):
+        self._items = {
+            STAT_HLTH: 100,
+            STAT_ENRG: 100,
+            STAT_AGIL: 10,
+            STAT_FLEX: 5,
+            STAT_STRN: 10,
+            STAT_SPED: 5,
+            STAT_LIBD: 10,
+            STAT_AROU: 0,
+            STAT_STAM: 50,
+        }
+    
+    
+    
+    
     
     
