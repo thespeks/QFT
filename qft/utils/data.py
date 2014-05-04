f809ee48ec2937e4f5d9ead39c79739b499604ea
TheSpeks <thespeks@users.noreply.github.com>
2014-05-04 05:13:37 -0400

    Update data.py
    write_attr_subdicts can handle x as a dict too
    added checks to avoid writing keys starting with '_'

--- qft/utils/data.py
+++ qft/utils/data.py
@@ -2,11 +2,18 @@
 
 # Magical Writing Stuff
 def write_attr_subdicts(f, prefix, x):
-    for da in x:        # class with dict type .attrs 
-        for keyname in iter(da):   
-            # write lines as: (without stuff in '()' ) 
-            #   'prefix.stuff.(x)name.keyname:(da[keyname)item
-            write_joined(f, (prefix,) + (x.name, keyname), da[keyname])
+    if isinstance(x, dict):
+        for da in x.values():         
+            for keyname in da.keys():
+                # write lines as: (without stuff in '()' ) 
+                #   'prefix.stuff.(x)name.keyname:(da[keyname)item
+                if da[keyname][0] != '_':
+                    write_joined(f, (prefix,) + (x.name, keyname), da[keyname])
+    else:
+        for da in x:        # class with dict type .attrs 
+            for keyname in da.keys():   
+                if da[keyname][0] != '_':
+                    write_joined(f, (prefix,) + (x.name, keyname), da[keyname])
 
 def write_joined(f, keynames, values, kv_sep=':', keyname_sep='. ',
     value_sep', '):
