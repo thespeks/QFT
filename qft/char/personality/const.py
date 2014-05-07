a25964cabec6abbc73f15ee3e0360c0e8442b12c
TheSpeks <thespeks@users.noreply.github.com>
2014-05-07 17:26:05 -0400

    Create const.py

---
+++ qft/char/personality/const.py
@@ -0,0 +1,32 @@
+
+
+
+
+# Personality attribute names
+
+FAV_COL =             'Favorite Color'
+
+
+# X
+FAV_POS =             'Favorite sex position'
+
+
+# Preferences -- (char._prefers)
+
+
+# will be in char._prefs or char._req (both, one or the other, or none)
+MIN_COCK_LEN =    1000   
+MAX_COCK_LEN =    1001
+MIN_COCK_THICK =  1002
+MAX_COCK_THICK =  1003
+
+MIN_KNOT_SIZE =   1010
+MAX_KNOT_SIZE =   1011
+
+MIN_COCK_MUSK =   1100
+MAX_COCK_MUSK =   1101
+
+
+
+MIN_BOOB_SIZE =   1200
+MAX_BOOB_SIZE =   1201
