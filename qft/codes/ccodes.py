4a6b3132019aaa139e8dc39430198497cfb90d97
TheSpeks <thespeks@users.noreply.github.com>
2014-05-14 20:45:21 -0400

    Create ccodes.py

---
+++ qft/codes/ccodes.py
@@ -0,0 +1,6 @@
+
+# class codes
+CCODES_CONFIG =     1000    # config
+CCODES_PARTY =      1010    # player party/chars in party
+CCODES_CHAR =       1011    # idividual char
+CCODES_CHARS =      1012    # all chars
