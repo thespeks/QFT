b7269d2eeae3bd4f89f668e3ad64c515fc98513f
TheSpeks <thespeks@users.noreply.github.com>
2014-05-14 20:50:34 -0400

    Create __init__.py

---
+++ qft/codes/__init__.py
@@ -0,0 +1,29 @@
+
+ALL_IDS =       20
+
+
+TYPE =                81
+SUBTYPE =             82
+DESCRIPTION = DESC =  83
+
+TAGS =                87
+FLAGS =               88
+ID =                  89
+
+SLOT =                97
+COLUMN =              98
+ROW =                 99
+
+NAME =                100
+HEIGHT =              101
+LENGTH =              102
+WIDTH =               103
+DEPTH =               104
+SIZE =                105
+LOC = LOCATION =      106
+ROT = ROTATION =      107
+SHAPE =               108
+
+
+COL = COLOR =         121
+COL2 = COLOR2 =       122
