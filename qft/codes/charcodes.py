9460fb11edca9ec92bd2ecc8340a3b4b4138b845
TheSpeks <thespeks@users.noreply.github.com>
2014-05-14 21:22:02 -0400

    Create charcodes.py

---
+++ qft/codes/charcodes.py
@@ -0,0 +1,17 @@
+# 200 - 
+STATUSES =          200
+SKILLS =            201
+
+
+# Most of these support at least LEVEL, MULT
+# Male -- Requires Balls
+VERI = VERILITY =   400   # (STATS, VERI, LEVEL) 0 is sterile
+RUT =               401
+
+# Female -- Requires Poon or PsudoPenis
+FERT = FERTILITY =  450   # (STATS, FERT, LEVEL) of 0 is sterile
+HEAT =              451
+PREG =              452
+
+# 
+LACT = LACTATION =  480
