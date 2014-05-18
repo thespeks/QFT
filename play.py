4c1439b68509a8b45e01409ba98f694c5084ae4a
TheSpeks <thespeks@users.noreply.github.com>
2014-05-18 15:16:09 -0400

    Update play.py
    added python version check

--- play.py
+++ play.py
@@ -1,3 +1,14 @@
+
+
+
 if __name__ == __main__:
-    import qft
-    qft.run
+    import sys
+    # Check were running the right version of python
+    if (sys.version_info.major == 3 and sys.version_info.minor >= 2):
+        import qft
+        qft.run
+    else:
+        sys.exit("""Invalid python version. (Got version {0}.{1})\n
+Requires python version 3.2 or greater.\n
+see: https://www.python.org/download/""".format(
+    sys.version_info.major, sys.version_info.minor))
