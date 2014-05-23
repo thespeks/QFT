f094f5ea77bc1fa03fc58fa2b851fb5d7f3be5fb
TheSpeks <thespeks@users.noreply.github.com>
2014-05-23 14:22:11 -0400

    Update ccodes.py

--- qft/codes/ccodes.py
+++ qft/codes/ccodes.py
@@ -4,3 +4,4 @@ CCODES_CONFIG =     1000    # config
 CCODES_PARTY =      1010    # player party/chars in party
 CCODES_CHAR =       1011    # idividual char
 CCODES_CHARS =      1012    # all chars
+CCODES_GAME =       1111    # Main Game
